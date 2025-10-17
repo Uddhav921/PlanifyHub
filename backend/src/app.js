5f94171a3662a5f808ae85fa4cfcf64f73b84ef6
Uddhav921 <uddhavtour6@gmail.com>
2025-10-17 15:09:56 +0530

    removed ejs & decided to use rest API json data for the backend which is suitable for react

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
+
 import { loggingMiddleware, nextMiddleware } from "./middlewares/appMiddleware.js";
 
 dotenv.config();
 
 const app = express();
-
-// EJS setup
-app.set("view engine", "ejs");
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.set("views", path.join(__dirname, "views")); // tell Express where views are
 
 // Middleware
 app.use(cors({
@@ -22,23 +16,14 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+/// use imported middlewares
 app.use(loggingMiddleware);
 app.use(nextMiddleware);
 
-// Serve static files from 'public' folder
-app.use(express.static(path.join(__dirname, "public")));
 
-// Route to render EJS page
+// Example route
 app.get("/", (req, res) => {
-  res.render("index");  // Express automatically looks for index.ejs in 'views'
-});
-
-app.get("/login", (req, res) => {
-  res.render("login"); // renders login.ejs
-});
-
-app.get("/create-event", (req, res) => {
-  res.render("create-event"); // renders create-event.ejs
+  res.send("Hello from PlanifyHub backend!");
 });
 
 export default app;
